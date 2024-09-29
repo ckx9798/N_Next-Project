dde8c4eb13c60eebef6a9b9c092817c73ca733fc
ckx9798 <ckx12@Naver.com>
2024-09-29 13:31:19 +0900

    Feat New페이지 추가

---
+++ app/new/page.js
@@ -0,0 +1,13 @@
+import { connectDB } from "@/util/database";
+import Link from "next/link";
+
+export default async function New() {
+  const db = (await connectDB).db("forum");
+  let result = await db.collection("post").find().toArray();
+
+  return (
+    <div className="list-bg">
+      <h4> New Item </h4>
+    </div>
+  );
+}

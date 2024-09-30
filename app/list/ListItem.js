4691e4d49eb144c692421456bf3f56ed94b13499
ckx9798 <ckx12@Naver.com>
2024-09-30 21:47:14 +0900

    Feat List페이지 삭제기능 추가

--- app/list/ListItem.js
+++ app/list/ListItem.js
@@ -1,4 +1,4 @@
-`use client`;
+"use client"; // 클라이언트 컴포넌트로 선언
 
 import Link from "next/link";
 
@@ -12,6 +12,18 @@ export default function ListItem({ result }) {
             <p>{item.content}</p>
           </Link>
           <Link href={`/edit/${item._id}`}>📌 수정하기</Link>
+          <br></br>
+          <div
+            onClick={() => {
+              fetch("/api/post/delete", {
+                method: "DELETE",
+                body: result[i]._id,
+              });
+            }}
+          >
+            🗑asdasd
+          </div>
+          <div onClick={() => console.log(1)}>asdasd </div>
         </div>
       ))}
     </div>
